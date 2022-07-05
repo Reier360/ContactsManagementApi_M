620fe4e3f7647a5270937f0aa0b0405bbcd13387
Rudi <reier360@gmail.com>
2022-07-05 23:57:44 +0200

    Cleanup and Controller add

---
+++ ContactsService/Controllers/ContactsController.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ContactsService.Controllers
+{
+    [Route("api/contacts")]
+    [ApiController]
+    public class ContactsController : ControllerBase
+    {
+        private readonly ILogger<ContactsController> _logger;
+
+        public ContactsController(ILogger<ContactsController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpPost]
+        public Object Add()
+        {
+            return Ok();
+        }
+
+        [HttpPut]
+        public Object Edit()
+        {
+            return Ok();
+        }
+
+        [HttpDelete]
+        public Object Delete()
+        {
+            return Ok();
+        }
+
+        [HttpGet]
+        public Object List()
+        {
+            return Ok();
+        }
+    }
+}
---
+++ ContactsService/Controllers/LoginController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ContactsService.Controllers
+{
+    [Route("api/login")]
+    [ApiController]
+    public class LoginController : ControllerBase
+    {
+        private readonly ILogger<LoginController> _logger;
+
+        public LoginController(ILogger<LoginController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpPost]
+        public Object Login()
+        {
+            return Ok();
+        }
+    }
+}
